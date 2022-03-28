6478eb438e7e9a5321928fe5d3f211bddacb3495
jangseoyoung <jangseoyoung0402@gmail.com>
2022-03-28 23:18:15 +0900

    [CREATE] UITextField Extension

---
+++ XD/XD/Extensions/UITextField.swift
@@ -0,0 +1,20 @@
+//
+//  UITextField.swift
+//  XD
+//
+//  Created by 장서영 on 2022/03/28.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+    func underLine() {
+        let border = CALayer()
+        border.frame = CGRect(x: 0, y: self.frame.size.height+5, width: self.frame.width, height: 1)
+        border.borderWidth = 1
+        border.borderColor = UIColor.lightGray.cgColor
+        self.layer.addSublayer(border)
+        self.adjustsFontSizeToFitWidth = true
+    }
+}
