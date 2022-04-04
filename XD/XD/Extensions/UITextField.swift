a451b3b9db29bc085c012e33e61112e732298512
jangseoyoung <jangseoyoung0402@gmail.com>
2022-04-04 23:40:36 +0900

    [FIX] UITextField Extension

--- XD/XD/Extensions/UITextField.swift
+++ XD/XD/Extensions/UITextField.swift
@@ -5,10 +5,11 @@
 //  Created by 장서영 on 2022/03/28.
 //
 
-import Foundation
 import UIKit
 
 extension UITextField {
+    
+    // SignUp, LoginViewController TextField UnderLine 효과
     func underLine() {
         let border = CALayer()
         border.frame = CGRect(x: 0, y: self.frame.size.height+5, width: self.frame.width, height: 1)
@@ -16,5 +17,15 @@ extension UITextField {
         border.borderColor = R.color.underLine()?.cgColor
         self.layer.addSublayer(border)
         self.adjustsFontSizeToFitWidth = true
+    }
+    
+    // WriteReviewController TextField Style
+    func basicTextFieldStyle() {
+        self.font = .systemFont(ofSize: 12)
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 10
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
     }
 }
