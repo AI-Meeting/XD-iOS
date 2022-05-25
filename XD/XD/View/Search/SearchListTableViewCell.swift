310b0d750a18720ca98e8e9cbbdebba59221abc3
jangseoyoung <jangseoyoung0402@gmail.com>
2022-05-25 14:45:26 +0900

    [FEAT] 삭제

--- XD/XD/View/Search/SearchListTableViewCell.swift
+++ XD/XD/View/Search/SearchListTableViewCell.swift
@@ -17,7 +17,8 @@ class SearchListTableViewCell: UITableViewCell {
         $0.backgroundColor = .white
         $0.layer.cornerRadius = 10
         $0.layer.shadowColor = UIColor.lightGray.cgColor
-        $0.layer.shadowOpacity = 0.2
+        $0.layer.shadowOpacity = 0.5
+        $0.layer.shadowOffset = CGSize(width: 0, height: 0)
     }
     
     let nameLabel = UILabel().then {
@@ -27,17 +28,125 @@ class SearchListTableViewCell: UITableViewCell {
     
     let jobLabel = UILabel().then {
         $0.font = .systemFont(ofSize: 10, weight: .medium)
-        $0.textColor = R.color.
+        $0.textColor = .lightGray
     }
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    
+    let locationLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 10, weight: .regular)
+        $0.textColor = R.color.textColor2()
     }
-
+    
+    let fieldLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 12, weight: .medium)
+        $0.textColor = R.color.textColor2()
+    }
+    
+    let questionCountLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 10, weight: .medium)
+        $0.textColor = R.color.textColor2()
+    }
+    
+    private let starImage = UIImageView().then {
+        $0.image = UIImage(systemName: "star.fill")
+        $0.tintColor = .systemYellow
+    }
+    
+    let scoreLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 18, weight: .medium)
+        $0.textColor = R.color.textColor2()
+    }
+    
+    private let reviewButton = UIButton(type: .system).then {
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = R.color.button1()
+        $0.titleLabel?.font = .systemFont(ofSize: 10, weight: .medium)
+        $0.setTitle("면접 후기 보러 가기", for: .normal)
+        $0.layer.cornerRadius = 5
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .white
+        setUpSubViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been impl")
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        if selected {
+            self.setSelected(false, animated: true)
+        }
     }
-
+    
+    // MARK: - public method
+    public func bind(name: String, job: String, location: String, field: String, questionCount: String, score: String) {
+        nameLabel.text = name
+        jobLabel.text = job
+        locationLabel.text = location
+        fieldLabel.text = field
+        questionCountLabel.text = questionCount
+        scoreLabel.text = score
+    }
+    
+    // MARK: - private method
+    
+    private func setUpSubViews() {
+        self.addSubview(contentView)
+        self.contentView.addSubview(cellBackgroundView)
+        
+        [nameLabel, jobLabel, locationLabel, fieldLabel, questionCountLabel, starImage, scoreLabel, reviewButton].forEach({self.cellBackgroundView.addSubview($0)})
+        
+        cellBackgroundView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+            $0.width.equalTo(333)
+            $0.height.equalTo(130)
+        }
+        
+        nameLabel.snp.makeConstraints {
+            $0.top.equalTo(14)
+            $0.leading.equalTo(16)
+        }
+        
+        jobLabel.snp.makeConstraints {
+            $0.leading.equalTo(nameLabel.snp.trailing).offset(2)
+            $0.bottom.equalTo(nameLabel.snp.bottom)
+        }
+        
+        locationLabel.snp.makeConstraints {
+            $0.top.equalTo(nameLabel.snp.bottom).offset(2)
+            $0.leading.equalTo(nameLabel.snp.leading)
+        }
+        
+        fieldLabel.snp.makeConstraints {
+            $0.top.equalTo(locationLabel.snp.bottom).offset(12)
+            $0.leading.equalTo(locationLabel.snp.leading)
+        }
+        
+        questionCountLabel.snp.makeConstraints {
+            $0.bottom.equalTo(-20)
+            $0.leading.equalTo(fieldLabel.snp.leading)
+        }
+        
+        starImage.snp.makeConstraints {
+            $0.trailing.equalTo(scoreLabel.snp.leading).offset(-4)
+            $0.top.equalTo(18)
+            $0.width.height.equalTo(20)
+        }
+        
+        scoreLabel.snp.makeConstraints {
+            $0.centerY.equalTo(starImage.snp.centerY)
+            $0.trailing.equalTo(-17)
+        }
+        
+        reviewButton.snp.makeConstraints {
+            $0.centerY.equalTo(questionCountLabel.snp.centerY)
+            $0.trailing.equalTo(-17)
+            $0.width.equalTo(122)
+            $0.height.equalTo(22)
+        }
+    }
 }
--- XD/XD/View/Search/SearchViewController.swift
+++ XD/XD/View/Search/SearchViewController.swift
@@ -6,23 +6,50 @@
 //
 
 import UIKit
+import SnapKit
 
 class SearchViewController: UIViewController {
-
+    
+    private let searchTableView = UITableView().then {
+        $0.backgroundColor = .clear
+        $0.separatorStyle = .none
+        $0.register(SearchListTableViewCell.self, forCellReuseIdentifier: "searchCell")
+        $0.rowHeight = 150
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
+        setUpSubViews()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func setUpSubViews() {
+        view.addSubview(searchTableView)
+        
+        searchTableView.snp.makeConstraints {
+            $0.width.height.equalToSuperview()
+        }
     }
-    */
+}
 
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "searchCell", for: indexPath) as? SearchListTableViewCell else { return UITableViewCell() }
+        
+        cell.bind(
+            name: "우아한 형제들",
+            job: "마케팅/IT",
+            location: "서울 강남구 역삼동",
+            field: "프론트엔드 분야",
+            questionCount: "질문 10개",
+            score: "3.9"
+        )
+        
+        return cell
+    }
 }
